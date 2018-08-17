3b8df092ecb1982fc7486a933c5e1b3263a8e8d4
lulasanchez <sanchezmarianelams@gmail.com>
2018-08-17 12:15:48 -0300

    Alta Usuario

---
+++ Proyectooficina/src/poo/muni/MuniAdministracion.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo.muni;
+import poo.muni.ui.AltaUsuario;
+/**
+ *
+ * @author oficina2
+ */
+public class MuniAdministracion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    AltaUsuario alta= new AltaUsuario();
+    alta.setVisible(true);
+    
+  
+    }
+    
+}
